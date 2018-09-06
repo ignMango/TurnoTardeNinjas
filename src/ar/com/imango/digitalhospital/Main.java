5e00db43ee005dc6e683587e1527fcd9927ecc4e
Imango <ignmango@gmail.com>
2018-09-06 16:29:10 -0300

    Borrando espacios.

--- src/ar/com/imango/digitalhospital/Main.java
+++ src/ar/com/imango/digitalhospital/Main.java
@@ -33,6 +33,8 @@ public class Main {
 
         p.cambiarEstado(pulso);
 
+        System.out.println("Holaaaaaaaa");
+
 
     }
 }
--- src/ar/com/imango/digitalhospital/Subscriptor.java
+++ src/ar/com/imango/digitalhospital/Subscriptor.java
@@ -1,7 +1,6 @@
 package ar.com.imango.digitalhospital;
 
 public interface Subscriptor {
-
     void eliminarMedidor(Medidor medidor);
     void agregarMedidor(Medidor medidor);
     void notificar(Medidor medidor);
